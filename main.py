4ee2e1cd151cfdf0f98524b093c47149614302e3
Manisha Varshney <varshney.manisha05@gmail.com>
2024-01-10 13:04:58 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from Project0 import logger
 from Project0.pipeline.stage0_data_ingestion import DataIngestionTrainingPipeline
+from Project0.pipeline.stage1_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/Project0/config/configuration.py
+++ src/Project0/config/configuration.py
@@ -1,6 +1,6 @@
 from Project0.constants import *
 from Project0.utils.common import read_yaml, create_directories
-from Project0.entity.config_entity import DataIngestionConfig
+from Project0.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -30,3 +30,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
