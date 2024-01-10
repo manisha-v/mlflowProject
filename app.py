6c7a9b01feb73caefe6347af22e5ec28cdb55b1c
Manisha Varshney <varshney.manisha05@gmail.com>
2024-01-10 21:41:26 +0530

    Web App update

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port = 8080, debug=True)
-	# app.run(host="0.0.0.0", port = 8080)
+	# app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080)
