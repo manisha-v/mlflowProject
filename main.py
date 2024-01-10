e05fb3080f0bb8fdc2ddbd3286aafbe7edc58e42
Manisha Varshney <varshney.manisha05@gmail.com>
2024-01-10 14:08:41 +0530

    Model Training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Project0 import logger
 from Project0.pipeline.stage0_data_ingestion import DataIngestionTrainingPipeline
 from Project0.pipeline.stage1_data_validation import DataValidationTrainingPipeline
 from Project0.pipeline.stage2_data_transformation import DataTransformationTrainingPipeline
+from Project0.pipeline.stage3_model_training import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -29,6 +30,17 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/Project0/pipeline/stage3_model_training.py
@@ -0,0 +1,27 @@
+from Project0.config.configuration import ConfigurationManager
+from Project0.components.model_training import ModelTrainer
+from Project0 import logger
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
